a8d100db9a4e385c8953223564cddf47dc4f05bf
alaagsaleh18 <123038492+alaagsaleh18@users.noreply.github.com>
2023-01-21 14:03:40 +0200

    Update app.js

--- js/app.js
+++ js/app.js
@@ -22,6 +22,13 @@
  * Define Global Variables
  * 
 */
+//call out for all 'section'
+const sections = document.querySelectorAll (section)
+
+//call for the id #nav_bar from the ul list to be used as a text for the link
+const list = getElementById('navbar__list');
+//create a document fragment to store in it the all added elements (li,a,textNode)
+const fragment = document.createDocumentFragment();
 
 
 /**
@@ -40,8 +47,48 @@
 
 // build the nav
 
+section.forEach(section => {
+    const sectionId = section.getAttribute("id")
+    const sectionTitle = section.getAttribute("data-nav");
+
+
+const listItem = document.createElement("li")
+const listLink = document.createElement("a")
+
+
+//add a,title,classattribute for links
+listLink.setAttribute (href, "#" + sectionId )
+listLink.textContent=sectionTitle;
+listLink.classList.add("menu__link");
+
+  
+  //add smooth scroll to links
+  listLink.addEventListener("Click" , evt => {
+    evt.preventDefault();
+    section.scrollIntoView({behavior:"smooth" })
+})
+listItem.appendChild(listLink);
+fragment.appendChild(listItem);
+}
+);
+list.appendChild(fragment)
+ 
 
 // Add class 'active' to section when near top of viewport
+
+const callBack =(entries) =>{
+    let section = entries[0]
+    section.classList.remove("your-active-class");
+    if(section.isIntersecting) {
+        section.classList.add("your-active-class");
+    }
+    else{
+        section.classList.remove("your-active-class");
+}
+}
+
+
+
 
 
 // Scroll to anchor ID using scrollTO event
@@ -57,6 +104,32 @@
 
 // Scroll to section on link click
 
+const observer = new IntersectionObserver(callBack,options);
+window.addEventListener("scroll"),evt => {
+    sections.forEach ( section => {
+        observer.observe(section);
+    })
+}
+
+
 // Set sections as active
+Window.addEventListener("scroll", evt => {
+    sections.forEach (section => {
+ const sectionTop = section.getBoundingClientRect ();
+ section.classList.remove("your-active-class")
+ 
+ if(sectionTop ==0 && sectionTop < 300) {
+    section.classList.add("your-active-class")
+    links.forEach( link => {
+        if ( link.textContent=== section.dataset.nav ) {
+            link.classList.add("active-link") }
+            else { link.classList.remove("active-link")}
+        })}
+        else { section.classList.remove("your-active-class") }
+    })
+})
+
+
+
 
 
